4060229a400a79ce823ea2e22efbd698f9bcf98c
Roger Rajaratnam <roger@sourcier.uk>
2018-03-19 17:43:44 +0000

    Fix bower components  🐿 v2.8.0

---
+++ secret-squirrel.js
@@ -0,0 +1,13 @@
+module.exports = {
+	files: {
+		allow: [],
+		allowOverrides: []
+	},
+	strings: {
+		deny: [],
+		denyOverrides: [
+			'220fd1a6-fe9c-11e6-96f8-3700c5664d30', // demos/fixtures.json:6|8|17|19|28|30|39|41
+			'9f416482-0dbe-11e8-839d-41ca06376bf2' // demos/fixtures.json:10|21|32|43
+		]
+	}
+};
